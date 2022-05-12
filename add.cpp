159d88732c9b1a842b1b0985c94bfd003e1768dc
mvalencia4578 <marissa.valencia4578@gmail.com>
2022-05-11 22:49:58 -0700

    Edited add branch's files (cpp, hpp, test)

--- add.cpp
+++ add.cpp
@@ -1,1 +1,15 @@
 //add class
+#include "add.hpp"
+
+Add:Add(Base* left, Base* right) : Base() {
+	this->left = left;
+	this->right = right;
+}
+
+double Add::evaluate() {
+	return this->left->evaluate() + this->right->evaluate();
+}
+
+std:string Add::stringify() {
+	return (this->left->stringify() + " + " + this->right->stringify());
+}
--- add_test.hpp
+++ add_test.hpp
@@ -1,1 +1,41 @@
 //testing add class
+
+#ifndef __CLASS_ADD_TEST__
+#define __CLASS_ADD_TEST__
+
+#include "gtest/gtest.h"
+
+#include "add.hpp"
+#include "op.hpp"
+
+TEST(ClassAdditionTest, AddEvaluateNonZero) {
+    Base* left = new Op(10.0);
+    Base* right = new Op(20.0);
+    Base* test = new Add(left, right);
+    EXPECT_DOUBLE_EQ(test->evaluate(), 30.0);
+}
+
+TEST(ClassAdditionTest, AddEvaluateNonZeroString) {
+    Base* left = new Op(10.0);
+    Base* right = new Op(20.0);
+    Base* test = new Add(left, right);
+    std::string str = "10.000000 + 20.000000";
+    EXPECT_EQ(test->stringify(), str);
+}
+
+TEST(ClassAdditionTest, AddEvaluatePosNeg) {
+    Base* left = new Op(-10.0);
+    Base* right = new Op(20.0);
+    Base* test = new Add(left, right);
+    EXPECT_DOUBLE_EQ(test->evaluate(), 10.0);
+}
+
+TEST(ClassAdditionTest, AddEvaluatePosNegString) {
+    Base* left = new Op(-10.0);
+    Base* right = new Op(20.0);
+    Base* test = new Add(left, right);
+    std::string str = "-10.000000 + 20.000000";
+    EXPECT_EQ(test->stringify(), str);
+}
+
+#endif
