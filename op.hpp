e8a82e6e44608e6ba0f5fe566e7276b4fab99712
sunnnytoast <evefraczkiewicz@gmail.com>
2022-05-11 23:12:23 -0700

    created op.hpp

--- op.hpp
+++ op.hpp
@@ -2,12 +2,19 @@
 #define __OP_HPP__
 
 #include "base.hpp"
+#include <string>
+
+using namespace std;
 
 class Op : public Base {
+    private:
+        double val = value;
+        string str = to_string(val);
+
     public:
         Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+        virtual double evaluate() { return val }
+        virtual string stringify() { return str }
 };
 
 #endif //__OP_HPP__
