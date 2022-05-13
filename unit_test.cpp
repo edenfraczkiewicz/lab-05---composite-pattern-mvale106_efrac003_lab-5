779bca683fa8422ddbb5931754ff6214d046bd7a
Eden Fraczkiewicz <102642562+edenfraczkiewicz@users.noreply.github.com>
2022-05-13 10:20:03 -0700

    Update unit_test.cpp

--- unit_test.cpp
+++ unit_test.cpp
@@ -5,7 +5,11 @@
 =======
 #include "op_test.hpp"
 #include "rand_test.hpp"
+#include "mult_test.hpp"
 #include "div_test.hpp"
+#include "add_test.hpp"
+#include "pow_test.hpp"
+#include "sub_test.hpp"
 >>>>>>> c38be38940f39bddcb85d0b1d280c231e508a222
 
 int main(int argc, char **argv) {
