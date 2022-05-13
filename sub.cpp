eb4427819860b858ca373151082caf2330753192
mvalencia4578 <marissa.valencia4578@gmail.com>
2022-05-12 18:47:58 -0700

    Edited sub branch's files (cpp, hpp, test)

--- sub.cpp
+++ sub.cpp
@@ -1,2 +1,16 @@
 //sub cpp
 
+#include "sub.hpp"
+
+Sub::Sub(Base* left, Base* right) : Base() {
+	this->left = left;
+  	this->right = right;
+}
+
+double Sub::evaluate() {
+  	return this->left->evaluate() - this->right->evaluate();
+}
+
+std::string Sub::stringify() {
+  	return (this->left->stringify() + " - " + this->right->stringify());
+}
