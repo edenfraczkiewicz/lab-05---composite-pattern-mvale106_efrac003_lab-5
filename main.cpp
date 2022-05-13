fff977204bf461174044071dabae13765ac0a8ed
mvalencia4578 <marissa.valencia4578@gmail.com>
2022-05-13 12:22:38 -0700

    fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 #include "base.hpp"
+#include "op.hpp"
+#include "rand.hpp"
+#include "add.hpp"
+#include "sub.hpp"
+#include "pow.hpp"
+#include "div.hpp"
+#include "mult.hpp"
 
 int main() {
     // This is a very basic main, and being able to correctly execute this main
